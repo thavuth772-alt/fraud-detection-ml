03c6aa22fa68cda410f490cc08472cd63ed4d91e
thavuth772-alt <thavuth772@gmail.com>
2026-04-27 15:41:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,24 @@ st.title("💳 Fraud Detection App")
 
 model = joblib.load("xgb_model.pkl")
 
-uploaded_file = st.file_uploader("Upload CSV")
+# Inputs
+amount = st.number_input("Amount")
+hour = st.number_input("Transaction Hour")
+foreign = st.selectbox("Foreign Transaction", [0,1])
+mismatch = st.selectbox("Location Mismatch", [0,1])
+trust = st.slider("Device Trust Score", 0.0, 1.0)
+velocity = st.number_input("Velocity Last 24h")
+age = st.number_input("Cardholder Age")
 
-if uploaded_file:
-    data = pd.read_csv(uploaded_file)
-    preds = model.predict(data)
-    data["Prediction"] = preds
-    st.write(data.head())
+if st.button("Predict"):
+    data = pd.DataFrame([[amount, hour, foreign, mismatch, trust, velocity, age]],
+                        columns=["amount","transaction_hour","foreign_transaction",
+                                 "location_mismatch","device_trust_score",
+                                 "velocity_last_24h","cardholder_age"])
+    
+    pred = model.predict(data)
+    
+    if pred[0] == 1:
+        st.error("⚠️ Fraud Detected")
+    else:
+        st.success("✅ Legit Transaction")
